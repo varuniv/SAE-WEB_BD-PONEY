1edca3568de0f369f3d4487d7437511ab1b4c1c8
Toniomp <delahayetonio77@gmail.com>
2025-01-06 17:29:43 +0100

    Pages login, accueil et header fonctionnelles

---
+++ web/accueil.php
@@ -0,0 +1,12 @@
+<?php
+$cssFile = "styles/accueil.css";
+include 'header.php';
+?>
+
+    <div class="accueil_div">
+        <button onclick="window.location.href='plannings.php'">Consulter les plannings de cours</button>
+        <button onclick="window.location.href='informations.php'">Consulter mes informations</button>
+    </div>
+</body>
+</body>
+</html>
