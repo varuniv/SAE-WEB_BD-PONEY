515a40580366d657eb664f0d128a82f88512d277
Toniomp <delahayetonio77@gmail.com>
2025-01-06 17:31:27 +0100

    rectification des pages

--- web/header.php
+++ web/header.php
@@ -2,22 +2,28 @@
 ?>
 
 <!doctype html>
-<html lang="fr">
+<html lang="fr" style="height:100%">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Grand Galop</title>
-    <link rel="stylesheet" href="styles/style.css">
+    <?php
+    // Inclure le fichier CSS spécifique à la page
+    if (isset($cssFile)) {
+        echo '<link rel="stylesheet" href="'.$cssFile.'">';
+    } else {
+        echo '<link rel="stylesheet" href="styles/accueil.css">';
+    }
+    ?>
 </head>
-<body>
+<body style="margin: 0;height:100%">
     <header>
         <nav>
-            <div id="header_barre">
-                <button class="header-btn">
+            <div style="display: flex;background-color: black;position: fixed;width: 100%;padding: 1rem 20px;">
+                <button style="padding: 10px;background-color: #F5DF4D;border-radius: 40%;">
                     <a href="login.php">Accueil</a>
                 </button>
             </div>
         </nav>
     </header>
-</body>
-
-</html>
--- web/login.php
+++ web/login.php
@@ -14,18 +14,17 @@ $loginInputs=array(
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" style="height:100%">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/styles.css">
-    <title>Grand Galop: Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/login.css">
+    <title>Grand Galop</title>
 </head>
-<body>
-    <?php
-    ?>
-    <div class="main">
-        <img src="img/loginImg.png" alt="Image de la page de connexion">
+<body class="h-100">
+    <div class="login_div">
+        <img src="img/poneyAccueil.png" alt="Image de la page de connexion">
         <div class="aside">
             <img src="img/loginTitle.png" alt="Titre:Grand Galop">
             <form action="login.php" method="POST">
