181847150f3e9a833ec2cde63a41c810845b1b2b
Toniomp <delahayetonio77@gmail.com>
2025-01-06 16:03:15 +0100

    Co-authored-by: AlexandreGuihard <AlexandreGuihard@users.noreply.github.com> Co-authored-by: Vardanyan Sargis <varuniv@users.noreply.github.com>

---
+++ bd/drop.sql
@@ -0,0 +1,9 @@
+-- Fichier pour drop les tables existantes
+
+drop table if exists RESERVER;
+drop table if exists COURS;
+drop table if exists CRENEAU;
+drop table if exists PONEY;
+drop table if exists MONITEUR;
+drop table if exists ADHERANT;
+drop table if exists PERSONNE;
---
+++ web/header.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Grand Galop</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div id="header_barre">
+                <button class="header-btn">
+                    <a href="login.php">Accueil</a>
+                </button>
+            </div>
+        </nav>
+    </header>
+</body>
+
+</html>
